e17e3a6e17aa4a23ae82f398c219557909e08458
Robert Graham-Hu <robertgrahamhu@gmail.com>
2021-05-05 11:48:27 -0400

    Added readonly attribute for editor

---
+++ kirby-birby-unity/Assets/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        string valueStr;
+
+        switch (prop.propertyType)
+        {
+            case SerializedPropertyType.Integer:
+                valueStr = prop.intValue.ToString();
+                break;
+            case SerializedPropertyType.Boolean:
+                valueStr = prop.boolValue.ToString();
+                break;
+            case SerializedPropertyType.Float:
+                valueStr = prop.floatValue.ToString("0.00000");
+                break;
+            case SerializedPropertyType.String:
+                valueStr = prop.stringValue;
+                break;
+            case SerializedPropertyType.Enum:
+                valueStr = prop.enumNames[prop.enumValueIndex];
+                break;
+            default:
+                valueStr = "(not supported)";
+                break;
+        }
+
+        EditorGUI.LabelField(position, label.text, valueStr);
+    }
+}
---
+++ kirby-birby-unity/Assets/ReadOnlyAttribute.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+// DO NOT MOVE
+// Required to be in top-level of Assets 
+// See ReadOnlyDrawer.cs for implementation
+
+/// <summary>
+/// Usage: [ReadOnly]
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
