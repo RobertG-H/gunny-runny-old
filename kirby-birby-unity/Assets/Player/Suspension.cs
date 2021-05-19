b77ae4af25b202a0df73214c2118d7411f5c5830
Robert Graham-Hu <robertgrahamhu@gmail.com>
2021-05-19 14:20:30 -0400

    Added suspension

---
+++ kirby-birby-unity/Assets/Player/Suspension.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Player
+{
+    /// <summary>
+    /// A spring/damper model for suspension
+    /// Src: https://www.youtube.com/watch?v=x0LUiE0dxP0
+    /// </summary>
+    public class Suspension : MonoBehaviour
+    {
+        [SerializeField] Rigidbody rb;
+        [SerializeField] Transform localBasisVectors;
+        [SerializeField] NetworkBehaviour networkParent;
+        [SerializeField] float restLength;
+        [SerializeField] float springTravel;
+        [SerializeField] float springStiffness;
+        [SerializeField] float damperStiffness;
+        float minLength;
+        float maxLength;
+        float lastLength;
+        float springLength;
+        float springVelocity;
+        float springForce;
+        float damperForce;
+
+        Vector3 suspensionForce;
+
+        [Header("Wheel")]
+        [SerializeField] float wheelRadius;
+
+        void Awake()
+        {
+            minLength = restLength - springTravel;
+            maxLength = restLength + springTravel;
+        }
+
+        void FixedUpdate()
+        {
+            if (!networkParent.isServer)
+            {
+                if (Physics.Raycast(transform.position, -localBasisVectors.up, out RaycastHit hit, maxLength + wheelRadius))
+                {
+                    lastLength = springLength;
+                    springLength = hit.distance - wheelRadius;
+                    springLength = Mathf.Clamp(springLength, minLength, maxLength);
+                    springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
+                    springForce = springStiffness * (restLength - springLength);
+                    damperForce = damperStiffness * springVelocity;
+
+                    suspensionForce = (springForce + damperForce) * hit.normal;
+
+                    rb.AddForceAtPosition(suspensionForce, hit.point);
+                }
+            }
+        }
+    }
+}
+
